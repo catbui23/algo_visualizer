ff4056627bceff9c3be7a96d4aa6c081220c12b6
gia-cat-bui-le <builegiacat@gmail.com>
2026-03-31 13:53:04 +0700

    Add Quick Sort Algorithm
    Signed-off-by: gia-cat-bui-le <builegiacat@gmail.com>

---
+++ src/algorithm/sorting/QuickSort.cpp
@@ -0,0 +1,56 @@
+#include "algoVisualizer/algorithm/sorting/implementations/QuickSort.hpp"
+#include "algoVisualizer/modules/sorting/sortingVisualizer.hpp"
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+QuickSort::QuickSort(const vector<unsigned int>& arr, SortingVisualizer& visualier)
+    : mArr(arr)
+    , mVisualizer(visualier)
+{
+}
+
+QuickSort::~QuickSort() { }
+
+void QuickSort::start()
+{
+    runStep();
+};
+
+void QuickSort::runStep()
+{
+    quickSort(0, mArr.size() - 1);
+}
+
+void QuickSort::quickSort(int l, int r)
+{
+    if (l >= r) {
+        return;
+    }
+    int pi = partition(l, r);
+    quickSort(l, pi - 1);
+    quickSort(pi + 1, r);
+}
+
+int QuickSort::partition(int l, int r)
+{
+    unsigned int pivot = mArr[r];
+    int i = l - 1;
+
+    for (int j = l; j <= r - 1; ++j) {
+        if (mArr[j] < pivot) {
+            i++;
+            mVisualizer.swapStep(i, j);
+            swap(mArr[i], mArr[j]);
+        }
+    }
+    mVisualizer.swapStep(i + 1, r);
+    swap(mArr[i + 1], mArr[r]);
+    return i + 1;
+}
+
+void QuickSort::end() const
+{
+    mVisualizer.printFinalFrame();
+}
