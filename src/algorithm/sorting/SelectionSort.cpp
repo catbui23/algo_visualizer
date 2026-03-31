74a4615552cd5f3085619efbbaa9e767fba9aafa
gia-cat-bui-le <builegiacat@gmail.com>
2026-03-31 11:50:45 +0700

    refactor Selection Sort to use new visualize methods
    Adapted Insertion Sort implementation to the refactored visualizer methods

--- src/algorithm/sorting/SelectionSort.cpp
+++ src/algorithm/sorting/SelectionSort.cpp
@@ -1,12 +1,15 @@
 #include "algoVisualizer/algorithm/sorting/implementations/SelectionSort.hpp"
-#include "algoVisualizer/algorithm/sorting/interface/SortStep.hpp"
 #include "algoVisualizer/modules/sorting/sortingVisualizer.hpp"
 #include <utility>
 #include <vector>
 
 using namespace std;
 
-SelectionSort::SelectionSort(const vector<unsigned int> &arr, SortingVisualizer &visualier) : mArr(arr), mVisualizer(visualier) {}
+SelectionSort::SelectionSort(const vector<unsigned int>& arr, SortingVisualizer& visualier)
+    : mArr(arr)
+    , mVisualizer(visualier)
+{
+}
 
 SelectionSort::~SelectionSort()
 {
@@ -20,23 +23,17 @@ void SelectionSort::start()
 void SelectionSort::runStep()
 {
     int n = mArr.size();
-    for (size_t i = 0; i < n - 1; ++i)
-    {
+    for (size_t i = 0; i < n - 1; ++i) {
         size_t min_idx = i;
-        for (size_t j = i + 1; j < n; ++j)
-        {
-            SortStep compareStep = {min_idx, j, StepType::COMPARE};
-            mVisualizer.updateSortStep(compareStep);
-            if (mArr[j] < mArr[min_idx])
-            {
+        for (size_t j = i + 1; j < n; ++j) {
+            mVisualizer.compareStep(min_idx, j);
+            if (mArr[j] < mArr[min_idx]) {
                 min_idx = j;
             }
         }
-        if (min_idx != i)
-        {
-            SortStep swapStep = {i, min_idx, StepType::SWAP};
-            mVisualizer.updateSortStep(swapStep);
+        if (min_idx != i) {
             swap(mArr[min_idx], mArr[i]);
+            mVisualizer.swapStep(min_idx, i);
         }
     }
 }
